aa53a1ff485f97fbb72dd425b366d5b8f1041939
MiroshnichenkoArtem <57839029+MiroshnichenkoArtem@users.noreply.github.com>
2020-12-09 00:38:24 +0300

    Initial commit

---
+++ CarPark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarPark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
